1ce4f9cc0617a0a104b3c6b714c83f1232799ee1
nechiporenko <s.nechiporenko@gmail.com>
2015-11-12 12:38:49 +0200

    fonts, slideout-core, color theme added

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 // Application Scripts:
 
-// Десктоп меню (выпадайки)
-// Мобильное меню
+
 // Сообщения об отправке формы
 // Кнопка скролла страницы
 // Модальное окно
@@ -12,50 +11,8 @@ jQuery(document).ready(function ($) {
     var $window = $(window),
         $body = $('body');
 
-    //
-    // Десктоп меню (выпадайки)
-    //---------------------------------------------------------------------------------------
-    var initDesktopMenu = (function () {
-        $('.js-menu li').on({
-            mouseenter: function () {
-                $(this).find('ul:first').stop(true, true).fadeIn('fast');
-                $(this).find('a:first').addClass('hover');
-            },
-            mouseleave: function () {
-                $(this).find('ul').stop(true, true).fadeOut('slow');
-                $(this).find('a:first').removeClass('hover');
-            }
-        })
-    })();
-
-    //
-    // Мобильное меню
-    //---------------------------------------------------------------------------------------
-    var initMobileMenu = (function () {
-        var $hidden = '<div class="g-hidden" id="menu_clone"></div>';//необходимо запустить мобильное меню, предварительно удалив из него css-классы
-        $body.append($hidden);//добавили временный скрытый блок
-        $('.js-menu').clone().appendTo($('#menu_clone'));//клонировали в него десктоп-меню
-        $('#menu_clone').find('ul, li').removeAttr('class');//убрали в нем классы десктоп-меню
-        $('#menu_clone a').each(function () {//пробежались по линкам
-            var isCurrent = false;
-            if ($(this).hasClass('menu__link--current') || ($(this).hasClass('submenu__link--current'))) {
-                isCurrent = true;//если содержит класс текущей страницы - изменили значение флага
-            }
-            $(this).removeAttr('class');//прибили в них классы десктоп-меню
-            if (isCurrent) {
-                $(this).addClass('current');//передали класс текущей страницы в мобильное меню
-            }
-        });
-        $('#menu_clone > ul').slicknav({//запускаем мобильное меню
-            label: '',
-            allowParentLinks: true,//разрешаем переход по линкам, содержащим под-меню
-            openedSymbol: '<i class="icon-down-dir"></i>',
-            closedSymbol: '<i class="icon-right-dir"></i>',
-            init: function () {
-                $('#menu_clone').remove(); //убиваем временный скрытый блок
-            }
-        });
-    })();
+    
+   
 
     //
     // Сообщения об отправке формы
@@ -168,11 +125,7 @@ jQuery(document).ready(function ($) {
         });
     }
 
-    //slider init
-    $('.js-slider').bxSlider({
-        infiniteLoop: false,
-        hideControlOnEnd:true
-    });
+    
 
     
 });
